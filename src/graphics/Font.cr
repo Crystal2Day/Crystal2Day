19d500b43d619ad6741e93c29c707d25dc4f9a92
Hadeweka <hadeweka@outlook.de>
2025-04-26 22:22:11 +0200

    Added some font options

--- src/graphics/Font.cr
+++ src/graphics/Font.cr
@@ -4,6 +4,11 @@ module Crystal2Day
   class Font
     Crystal2DayHelper.wrap_type(Pointer(LibSDL::TTFFont))
 
+    getter outline_thickness : UInt32 = 0
+    getter size : Float32 = 0.0
+
+    # TODO: Add font style
+
     def self.default_font_path
       # NOTE: You should always use your own fonts to prevent this method from failing
       {% if flag?(:win32) %}
@@ -33,17 +38,30 @@ module Crystal2Day
 
       @data = LibSDL.ttf_open_font(full_filename, size)
       Crystal2Day.error "Could not font from file #{full_filename}" unless @data
+
+      @size = LibSDL.ttf_get_font_size(data)
     end
 
     def free
       if @data
         LibSDL.ttf_close_font(data)
+        @data = nil
       end
     end
 
     def calculate_text_rect(text : String)
       LibSDL.ttf_size_text(@data, text, out w, out h)
       Crystal2Day::Rect.new(0, 0, w, h)
+    end
+
+    def outline_thickness=(value : Int)
+      LibSDL.ttf_set_font_outline(data, value)
+      @outline_thickness = value.to_u32
+    end
+
+    def size=(value : Number)
+      LibSDL.ttf_set_font_size(data, value)
+      @size = value.to_f32
     end
 
     def finalize
